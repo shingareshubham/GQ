ddcc1f7323680bde3f6a4ee3f2a0a5f3be38dd78
shubham <shingareshubham@gmail.com>
2021-01-01 23:42:22 +0530

    Added API logic to read url

--- photoGallary/views.py
+++ photoGallary/views.py
@@ -7,5 +7,11 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='loginPage')
 def homepage(request):
     """Render HTTML page"""
-    context = photo.objects.all()
+    # context = {}
+    #context['data'] = photo.objects.all()
+    response = requests.get('http://www.scloud24.com/api/photogallary/v1/getall/')
+    context = response.json()
+    # if request.method == 'POST':
+    #     value = request.POST.get('value')
+
     return render(request, 'gallery.html', {'data': context})
